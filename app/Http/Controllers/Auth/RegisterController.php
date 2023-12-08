850e73f882c7b90c0cf67ab0c7720d1328db8acb
Kazantsev Oleg <kazoleg@gmail.com>
2023-12-08 13:18:18 +0300

    08.12

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -11,8 +11,18 @@ use Illuminate\Http\Request;
 class RegisterController extends Controller
 {
     protected $rules = [
-        'email' => 'required|string|email',
+        'name' => 'required|min:5',
+        'email' => 'required|string|email|unique:users',
         'password' => 'required|string',
+    ];
+
+    protected $messages = [
+        'name.required' => 'Пожалуйста, введите имя',
+        'name.min' => 'Минимальная длина имени 5 символов',
+        'password.required' => 'Пожалуйста, введите пароль',
+        'email.unique' => 'Пользователь с таким email уже зарегистрирован',
+        'email.required' => 'Пожалуйста, введите E-mail',
+        'email.email' => 'Пожалуйста, введите корректный E-mail',
     ];
 
     /**
@@ -34,38 +44,44 @@ class RegisterController extends Controller
     // Добавьте метод register, даже если он пустой
     public function register(Request $request)
     {
-        // Создание нового пользователя
-        if (empty($request->input('agent'))) {
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => bcrypt($request->password),
-                'status' => 1
-            ]);
-        } else {
+        //валидация данных формы
+        $validator = Validator::make($request->all(), $this->rules, $this->messages);
+        if ($validator->passes()) {
+            // Создание нового пользователя
+            if (!$request->has('agent')) {
+                $user = User::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => bcrypt($request->password),
+                    'status' => 1
+                ]);
+            } else {
+                $user = User::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => bcrypt($request->password),
+                    'status' => 0,
+                    'users_group' => 2,
+                    'sad_id' => $request->input('item_id')
+                ]);
+            }
+            // Отправка уведомления о подтверждении email
+            $user->sendEmailVerificationNotification();
 
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => bcrypt($request->password),
-                'status' => 0,
-                'users_group' => 2,
-                'sad_id' => $request->input('item_id')
-            ]);
+            // Вход после регистрации
+            Auth::login($user);
+            if (!empty($request->input('agent'))) {
+                $data = ['status' => 1, 'msg' => 'Вам на почту ушло письмо, перейди по ссылке в нем для подтверждения email.'];
+                return response()->json($data);
+            } else {
+                return redirect('/verification-message'); // Перенаправление после успешной регистрации
+            }
+        }else{
+            return redirect()
+                ->back()
+                ->withErrors($validator)
+                ->withInput();
         }
-
-        // Отправка уведомления о подтверждении email
-        $user->sendEmailVerificationNotification();
-
-        // Вход после регистрации
-        Auth::login($user);
-        if (!empty($request->input('agent'))) {
-            $data = ['status' => 1, 'msg' => 'Вам на почту ушло письмо, перейди по ссылке в нем для подтверждения email.'];
-            return response()->json($data);
-        } else {
-            return redirect('/verification-message'); // Перенаправление после успешной регистрации
-        }
-
     }
 
 
---
+++ tests/Feature/RegistrationTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    use DatabaseTransactions;
+    use WithFaker;
+
+    /** @test */
+    public function user_can_register()
+    {
+        // тестовые данные
+        $userData = [
+            'name' => 'John Doe',
+            'email' => 'ltpm@ya.ru',
+            'password' => 'password123',
+            'password_confirmation' => 'password123',
+        ];
+
+        // Посылка POST-запроса на страницу регистрации
+        $response = $this->post('/register', $userData);
+
+        // проверки
+        $response->assertStatus(302);
+        $this->assertAuthenticated();
+    }
+
+    /** @test */
+    public function registration_requires_all_fields()
+    {
+        // Посылка POST-запроса на страницу регистрации с пустыми полями
+        $response = $this->post('/register', []);
+
+        // Проверка статуса ответа (должен быть 422 для ошибки валидации)
+        $response->assertStatus(422);
+
+        // Проверка, что в ответе есть сообщения об ошибках для каждого поля
+        $response->assertJsonValidationErrors([
+            'name', 'email', 'password',
+        ]);
+    }
+}
