c3480a7afa29b0af780a055694782b9f894f4ee6
Kazantsev Oleg <kazoleg@gmail.com>
2023-12-08 13:50:38 +0300

    08.12

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -34,17 +34,14 @@ class RegistrationTest extends TestCase
     public function registration_requires_all_fields()
     {
         // Посылка POST-запроса на страницу регистрации с пустыми полями
-        $response = $this->post('/register', ['name' => 'John',]);
+        $response = $this->post('/register', []);
 
         // Проверка статуса ответа (должен быть 302 для ошибки валидации)
         $response->assertStatus(302);
 
-        // Проверяем, что редирект идет на ожидаемый URL
-        $response->assertRedirect('/register');
-
         // Проверка, что в ответе есть сообщения об ошибках для каждого поля
         $response->assertJsonValidationErrors([
-            'name'
+            'name', 'email', 'password',
         ]);
     }
 }
