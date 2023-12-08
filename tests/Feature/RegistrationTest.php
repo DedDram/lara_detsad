031bab52c0c78e325ef9cb77f91eb2d3b3f606ba
Kazantsev Oleg <kazoleg@gmail.com>
2023-12-08 13:24:53 +0300

    08.12

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -39,6 +39,9 @@ class RegistrationTest extends TestCase
         // Проверка статуса ответа (должен быть 302 для ошибки валидации)
         $response->assertStatus(302);
 
+        // Проверяем, что редирект идет на ожидаемый URL
+        $response->assertRedirect('/register');
+
         // Проверка, что в ответе есть сообщения об ошибках для каждого поля
         $response->assertJsonValidationErrors([
             'name', 'email', 'password',
