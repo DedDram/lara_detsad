6e96569ac0e8ef3b4579ee283284ea579aaa48b3
Kazantsev Oleg <kazoleg@gmail.com>
2023-12-24 09:56:38 +0300

    24.12

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group([
-    'prefix' => 'v1',
-], function () {
-    // Ваши маршруты API здесь
-    Route::get('/test',  [ApiController::class, 'test']);
-});
+Route::get('/test',  [ApiController::class, 'test']);
+
